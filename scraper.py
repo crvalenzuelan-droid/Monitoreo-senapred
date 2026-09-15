4c9debdb9ca60869e30e995ce4f562e1850598ed
crvalenzuelan-droid <crvalenzuela.n@gmail.com>
2026-09-15 20:55:26 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,14 @@
-print("SENAPRED OK")
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+
+    page = browser.new_page()
+
+    page.goto("https://www.senapred.cl/alertas")
+
+    page.wait_for_timeout(10000)
+
+    print(page.title())
+
+    browser.close()
