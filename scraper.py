0f82a464a1ce8f88b6c7c53854922c20bf9404bd
crvalenzuelan-droid <crvalenzuela.n@gmail.com>
2026-09-16 23:14:21 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,17 +6,26 @@ with sync_playwright() as p:
 
     page = browser.new_page()
 
-    page.goto(
-        "https://www.senapred.cl/alerta/se-declara-alerta-temprana-preventiva-para-la-provincia-de-el-loa-y-las-comunas-de-antofagasta-y-taltal-por-nevadas-2026-09-16-18-55-04"
-    )
+    page.goto("https://www.senapred.cl/alertas")
 
     page.wait_for_timeout(10000)
 
-    html = page.content()
-
-    with open("detalle.html", "w", encoding="utf-8") as f:
-        f.write(html)
-
-    print("HTML GUARDADO")
+    enlaces = page.locator("a").evaluate_all("""
+        links => links.map(link => ({
+            texto: link.innerText,
+            href: link.href
+        }))
+    """)
 
     browser.close()
+
+    with open("enlaces.txt", "w", encoding="utf-8") as f:
+
+        for e in enlaces:
+
+            if e["href"] and "alerta" in e["href"]:
+
+                f.write(f"{e['texto']} | {e['href']}\n")
+                print(e["texto"])
+                print(e["href"])
+                print("-" * 50)
