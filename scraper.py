905cad340dbef1d826bb4e4ded3cdfe3b03eb567
crvalenzuelan-droid <crvalenzuela.n@gmail.com>
2026-09-16 23:39:33 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,9 @@
 from playwright.sync_api import sync_playwright
+from datetime import datetime, timezone
 import json
+import re
 
-detalles = []
+alertas = []
 
 with sync_playwright() as p:
 
@@ -48,33 +50,73 @@ with sync_playwright() as p:
 
             texto = detalle_page.locator("body").inner_text()
 
-            detalles.append({
-                "url": url,
-                "contenido": texto[:8000]
-            })
+            lineas = [x.strip() for x in texto.split("\n") if x.strip()]
 
-            print("OK:", url)
+            titulo = ""
+            fecha = ""
+            accion = ""
+            tipo = ""
+            region = ""
 
-            detalle_page.close()
+            for linea in lineas:
 
-        except Exception as e:
+                if (
+                    linea.startswith("Monitoreo")
+                    or linea.startswith("Se declara")
+                    or linea.startswith("Se modifica")
+                    or linea.startswith("Se cancela")
+                    or linea.startswith("Se actualiza")
+                ):
 
-            print("ERROR:", url)
-            print(str(e))
+                    titulo = linea
 
-    browser.close()
+                    if linea.startswith("Monitoreo"):
+                        accion = "Monitoreo"
 
-with open(
-    "alertas_detalle.json",
-    "w",
-    encoding="utf-8"
-) as f:
+                    elif linea.startswith("Se declara"):
+                        accion = "Se declara"
 
-    json.dump(
-        detalles,
-        f,
-        ensure_ascii=False,
-        indent=2
-    )
+                    elif linea.startswith("Se modifica"):
+                        accion = "Se modifica"
 
-print("JSON generado")
+                    elif linea.startswith("Se cancela"):
+                        accion = "Se cancela"
+
+                    elif linea.startswith("Se actualiza"):
+                        accion = "Se actualiza"
+
+                    break
+
+            fecha_regex = re.search(
+                r"\d{2}-\d{2}-\d{4}\s\d{2}:\d{2}",
+                texto
+            )
+
+            if fecha_regex:
+                fecha = fecha_regex.group()
+
+            if "Alerta Roja" in titulo:
+                tipo = "Roja"
+
+            elif "Alerta Amarilla" in titulo:
+                tipo = "Amarilla"
+
+            elif "Temprana Preventiva" in titulo:
+                tipo = "ATP"
+
+            else:
+                tipo = "No definido"
+
+            if "para la Región de " in titulo:
+
+                region = (
+                    titulo
+                    .split("para la Región de ")[1]
+                    .split(" por ")[0]
+                )
+
+            elif "para las comunas de " in titulo:
+
+                region = (
+                    titulo
+                    .split("para 
